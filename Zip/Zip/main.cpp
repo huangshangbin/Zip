dc914308b5ad15c2d404af2fa8164e76e0b30693
huangshangbin <1661729343@qq.com>
2020-11-19 14:45:06 +0800

    complete

--- Zip/Zip/main.cpp
+++ Zip/Zip/main.cpp
@@ -2,12 +2,18 @@
 #include <zip/ZipUtils.hpp>
 
 
+//接下来   压缩文件夹，  解压压缩文件
+
 int main()
 {
-	ZipUtils::compressFile("fileCompress.zip", "E:\\group.txt");
+// 	ZipUtils::compressFile("fileCompress.zip", "E:\\group.txt");
+// 
+// 	string data = "huang\nshang\nbin";
+// 	ZipUtils::compressData("dataCompress.zip", "dataCompress.txt", data);
 
-	string data = "huang\nshang\nbin";
-	ZipUtils::compressData("dataCompress.zip", "dataCompress.txt", data);
+//	ZipUtils::decompression("dataCompress.zip", "E:\\zip");
+
+	ZipUtils::compressDir("dir.zip", "E:\\zip");
 
 	cout << "complete" << endl;
 
--- Zip/Zip/zip/ZipUtils.hpp
+++ Zip/Zip/zip/ZipUtils.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <io.h>
+
 #include <iostream>
 #include <string>
 
@@ -21,6 +23,15 @@ public:
 		zip_close(zip);
 	}
 
+	static void compressDir(string targetFilePath, string dirPath)
+	{
+		zip_t* zip = zip_open(targetFilePath.c_str(), ZIP_DEFAULT_COMPRESSION_LEVEL, 'w');
+
+		compressDirHelp(zip, dirPath, dirPath);
+
+		zip_close(zip);
+	}
+
 	static void compressData(string targetFilePath, string dataFileName, string& data)
 	{
 		zip_t * zip = zip_open(targetFilePath.c_str(), ZIP_DEFAULT_COMPRESSION_LEVEL, 'w');
@@ -32,7 +43,15 @@ public:
 		zip_close(zip);
 	}
 
+	static void decompression(string zipFilePath, string targetPath)
+	{
+		int arg = 2;
+		zip_extract(zipFilePath.c_str(), targetPath.c_str(), [](const char* fileName, void* arg) {
+			return 0;
+		}, &arg);
+	}
 
+//compressFile
 private:
 	static string getFileOrDirName(string path)
 	{
@@ -53,6 +72,49 @@ private:
 
 		string result = path.substr(index + 1, path.length() - index);
 		return result;
+	}
+
+
+//compressDir
+private:
+	static void compressDirHelp(zip_t* zip, string baseDirPath, string compressdDirPath)
+	{
+		string path = compressdDirPath + "\\*.*";
+
+		_finddata_t findData;
+		intptr_t findHandle = _findfirst(path.c_str(), &findData);
+		intptr_t flag = findHandle;
+
+		string dirName;
+		while (flag != -1)
+		{
+			if (findData.attrib == _A_SUBDIR)
+			{
+				dirName = findData.name;
+				if (dirName != "." && dirName != "..")
+				{
+					string newDirPath = compressdDirPath + "\\" + dirName;
+
+					compressDirHelp(zip, baseDirPath, newDirPath);
+				}
+			}
+			else
+			{
+				string relativePath = compressdDirPath.substr(baseDirPath.length(), compressdDirPath.length()) + 
+					"\\" + findData.name;
+				relativePath = getFileOrDirName(baseDirPath) + "\\" + relativePath;
+
+				string filePath = compressdDirPath + "\\" + findData.name;
+
+				zip_entry_open(zip, relativePath.c_str());//write target file path
+				zip_entry_fwrite(zip, filePath.c_str());//src file path
+				zip_entry_close(zip);
+			}
+
+			flag = _findnext(findHandle, &findData);
+		}
+
+		_findclose(findHandle);
 	}
 };
 
